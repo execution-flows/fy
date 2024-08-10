35063da63027dfff0bef9cae838ab276db9758f9
Jere <117933956+PerisicJere@users.noreply.github.com>
2024-08-10 14:49:15 -0400

    updated parser to for the latest syntax (#11)
    Co-authored-by: Vinko Buble <vinkobuble@gmail.com>

--- library/tests/test_hello_world_fy.py
+++ library/tests/test_hello_world_fy.py
@@ -1,3 +1,4 @@
+import os
 from unittest import TestCase
 
 from entry.main_flow import Main_Flow
@@ -5,12 +6,20 @@ from entry.main_flow import Main_Flow
 
 class TestHelloWorldFy(TestCase):
     def test_hello_world_generates_file(self) -> None:
-        # TODO: delete `hello_world.py` if exists
+
+        folder_to_parse = "tests/test_fy_files/hello_world"
+        file_to_generate = "hello_world.py"
+
+        path_file_to_generate = os.path.join(folder_to_parse, file_to_generate)
+
+        if os.path.exists(path):
+            os.remove(path)
+
         Main_Flow(
-            folder_to_parse="tests/test_fy_files/hello_world",
+            folder_to_parse=folder_to_parse,
         )()
 
-        with open("library/tests/test_fy_files/hello_world/hello_world.py", "r") as generated_py_file:
+        with open(path, "r") as generated_py_file:
             expected = ['from base.execution_flow_base import ExecutionFlowBase', '', '',
                         'class HelloWorld_Flow(ExecutionFlowBase[None]):', '    def __call__(self) -> None:',
                         '        print("Hello world!")', '']
