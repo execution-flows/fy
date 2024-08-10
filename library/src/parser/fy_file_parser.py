632874624cac0e28fdc9aa96a2b5bc4384b338d7
PerisicJere <117933956+PerisicJere@users.noreply.github.com>
2024-08-10 19:36:51 -0400

    Changed imports and variables from type to kind

--- library/src/parser/fy_file_parser.py
+++ library/src/parser/fy_file_parser.py
@@ -2,7 +2,7 @@ import re
 from pathlib import Path
 
 from domain.flow_template_model import FlowTemplateModel
-from domain.parsed_fy_file import ParsedFyFile, ParsedFyFileType
+from domain.parsed_fy_file import ParsedFyFile, ParsedFyFileKind
 
 
 class FyFileParser:
@@ -21,7 +21,7 @@ class FyFileParser:
         assert flow_fy_search is not None
 
         parsed_fy_file = ParsedFyFile(
-            file_type=ParsedFyFileType.FLOW,
+            file_type=ParsedFyFileKind.FLOW,
             output_py_file_path=file_path.with_name(f"{file_path.stem}.py"),
             template_model=FlowTemplateModel(
                 flow_name=flow_fy_search.group("flow_name"),
