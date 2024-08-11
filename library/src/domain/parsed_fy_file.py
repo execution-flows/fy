dee1a5211560f0a10898ee8e4cc63016a8124c5c
Jere <117933956+PerisicJere@users.noreply.github.com>
2024-08-11 15:14:10 -0400

    Added ParsedFlowFyFile class (#25)

--- library/src/domain/parsed_fy_file.py
+++ library/src/domain/parsed_fy_file.py
@@ -1,14 +1,28 @@
 from enum import Enum
 from pathlib import Path
+from typing import Any
 
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
+
+from domain.flow_template_model import FlowTemplateModel
 
 
-class ParsedFyFileType(Enum):
+class ParsedFyFileKind(Enum):
     FLOW = "flow"
 
 
 class ParsedFyFile(BaseModel):
-    file_type: ParsedFyFileType
+    file_type: ParsedFyFileKind
     output_py_file_path: Path
     template_model: BaseModel
+
+
+class ParsedFlowFyFile(ParsedFyFile):
+    template_model: FlowTemplateModel
+
+    @model_validator(mode="before")
+    @classmethod
+    def set_kind(cls, data: Any) -> Any:
+        if isinstance(data, dict):
+            data["kind"] = ParsedFyFileKind.FLOW
+        return data
--- library/src/mixins/method/generate_py_files.py
+++ library/src/mixins/method/generate_py_files.py
@@ -2,7 +2,7 @@ import abc
 
 from jinja2 import Environment, FileSystemLoader
 
-from domain.parsed_fy_file import ParsedFyFileType
+from domain.parsed_fy_file import ParsedFyFileKind
 from mixins.property.parsed_fy_files.abc import With_ParsedFyFiles_PropertyMixin_ABC
 
 
@@ -13,7 +13,7 @@ class GeneratePyFiles_MethodMixin(
     def _generate_py_files(self) -> None:
         for parsed_fy_file in self._parsed_fy_files:
             match parsed_fy_file.file_type:
-                case ParsedFyFileType.FLOW:
+                case ParsedFyFileKind.FLOW:
                     env = Environment(loader=FileSystemLoader("templates"))
                     template = env.get_template("flow.jinja2")
                     content = template.render(parsed_fy_file.template_model.model_dump())
