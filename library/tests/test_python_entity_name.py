e0c527288dd66503f810ac61356cd3813caba52e
PerisicJere <117933956+PerisicJere@users.noreply.github.com>
2024-08-11 16:13:22 -0400

    changed snake_case_expected to snake_case

--- library/tests/test_python_entity_name.py
+++ library/tests/test_python_entity_name.py
@@ -5,10 +5,10 @@ from domain.python_entity_name import PythonEntityName
 
 class TestPythonEntityName(TestCase):
     def test_python_entity_name(self) -> None:
-        snake_case_expected = "phrase_one__phrase_two"
+        snake_case = "phrase_one__phrase_two"
         pascal_case_expected = "PhraseOne_PhraseTwo"
-
-        generated_actual_cases = PythonEntityName.from_snake_case(snake_case_expected)
+        snake_case_expected = snake_case
+        generated_actual_cases = PythonEntityName.from_snake_case(snake_case)
 
         self.assertEqual(snake_case_expected, generated_actual_cases.snake_case)
         self.assertEqual(pascal_case_expected, generated_actual_cases.pascal_case)
