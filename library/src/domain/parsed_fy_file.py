722cbb92b7e6f7dfe60a0965d10c1ef4718ed6dd
PerisicJere <117933956+PerisicJere@users.noreply.github.com>
2024-08-12 21:32:22 -0400

    added license header to python files

--- library/src/domain/parsed_fy_file.py
+++ library/src/domain/parsed_fy_file.py
@@ -1,10 +1,13 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 from enum import Enum
 from pathlib import Path
 from typing import Any
 
 from pydantic import BaseModel, model_validator
 
-from domain.python_entity_name import PythonEntityName
 from domain.template_models import FlowTemplateModel, AbstractPropertyTemplateModel, PropertyTemplateModel
 
 
--- library/src/fy_library/__init__.py
+++ library/src/fy_library/__init__.py
@@ -1,1 +1,3 @@
-from entry.main_flow import Main_Flow
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
