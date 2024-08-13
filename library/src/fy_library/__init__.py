6ec47d7b1c85bc3becf6331f51ca8195698b97d3
Jere <117933956+PerisicJere@users.noreply.github.com>
2024-08-13 10:36:24 -0400

    add license and header to every python file (#38)

--- library/src/fy_library/__init__.py
+++ library/src/fy_library/__init__.py
@@ -1,1 +1,9 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+# This is the exported file from the library when installed as a package in other projects. Whatever is imported in
+# this file is accessible in projects that install `fy_library` package. Currently, CLI tool uses `Main_Flow` to run
+# the tool.
+
 from entry.main_flow import Main_Flow
