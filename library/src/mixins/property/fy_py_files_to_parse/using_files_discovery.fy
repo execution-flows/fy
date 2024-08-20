e7240dd5e6bf7b0bab0d033ac5d1a6f28457a619
Jere <117933956+PerisicJere@users.noreply.github.com>
2024-08-19 21:34:16 -0400

    implemented _fy_py_files_to_parse logic (#108)

--- library/src/mixins/property/fy_py_files_to_parse/using_files_discovery.fy
+++ library/src/mixins/property/fy_py_files_to_parse/using_files_discovery.fy
@@ -1,10 +1,21 @@
 from pathlib import Path
 from typing import List
 
+FY_PY_FILE_SIGNATURE = '"""fy\n'
 
 property fy_py_files_to_parse using files_discovery:
     with property folder_to_parse
 
     @cached
     def -> List[Path]:
-        return []
+        fy_py_files_in_directory = list(self._folder_to_parse.rglob("*.fy.py"))
+        fy_py_files: List[Path] = []
+
+        for fy_py_file in fy_py_files_in_directory:
+            with open(file=fy_py_file, mode='r') as file:
+                first_six_bytes = file.read(len(FY_PY_FILE_SIGNATURE))
+                if first_six_bytes == FY_PY_FILE_SIGNATURE:
+                    fy_py_files.append(fy_py_file)
+                else:
+                    raise SyntaxError(f"File {fy_py_file} does not start with {FY_PY_FILE_SIGNATURE}")
+        return fy_py_files
--- library/src/mixins/property/fy_py_files_to_parse/using_files_discovery.py
+++ library/src/mixins/property/fy_py_files_to_parse/using_files_discovery.py
@@ -7,6 +7,8 @@ from mixins.property.folder_to_parse.abc import With_FolderToParse_PropertyMixin
 from pathlib import Path
 from typing import List
 
+FY_PY_FILE_SIGNATURE = '"""fy\n'
+
 
 class FyPyFilesToParse_UsingFilesDiscovery_PropertyMixin(
     # Property_mixins
@@ -15,4 +17,16 @@ class FyPyFilesToParse_UsingFilesDiscovery_PropertyMixin(
 ):
     @cached_property
     def _fy_py_files_to_parse(self) -> List[Path]:
-        return []
+        fy_py_files_in_directory = list(self._folder_to_parse.rglob("*.fy.py"))
+        fy_py_files: List[Path] = []
+
+        for fy_py_file in fy_py_files_in_directory:
+            with open(file=fy_py_file, mode="r") as file:
+                first_six_bytes = file.read(len(FY_PY_FILE_SIGNATURE))
+                if first_six_bytes == FY_PY_FILE_SIGNATURE:
+                    fy_py_files.append(fy_py_file)
+                else:
+                    raise SyntaxError(
+                        f"File {fy_py_file} does not start with {FY_PY_FILE_SIGNATURE}"
+                    )
+        return fy_py_files
