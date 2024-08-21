02c046b5d71df41541595d0dea8987c54358e89c
Jere <117933956+PerisicJere@users.noreply.github.com>
2024-08-21 19:52:53 -0400

    simple parsing of the flow (#119)

--- library/src/domain/parsed_fy_py_file.py
+++ library/src/domain/parsed_fy_py_file.py
@@ -1,8 +1,12 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+from typing import Literal
+
 from pydantic import BaseModel
 from enum import Enum
+
+from domain.fy_py_template_models import FlowTemplateModel, BaseTemplateModel
 
 
 class ParsedFyPyFileKind(Enum):
@@ -10,4 +14,10 @@ class ParsedFyPyFileKind(Enum):
 
 
 class ParsedFyPyFile(BaseModel):
-    pass
+    file_type: ParsedFyPyFileKind
+    template_model: BaseTemplateModel
+
+
+class ParsedFlowFyPyFile(ParsedFyPyFile):
+    file_type: Literal[ParsedFyPyFileKind.FLOW] = ParsedFyPyFileKind.FLOW
+    template_model: FlowTemplateModel
