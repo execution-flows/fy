88968b0013bab147fc60ebdc66fb90c8f8a371b9
Jere <117933956+PerisicJere@users.noreply.github.com>
2024-08-27 15:53:51 -0400

    implemented parsed_abstract_property_fy_py_file (#184)
    Signed-off-by: Jere <117933956+PerisicJere@users.noreply.github.com>

--- library/src/flows/parse_abstract_property_fy_code_fy.py
+++ library/src/flows/parse_abstract_property_fy_code_fy.py
@@ -8,15 +8,16 @@ flow ParseAbstractPropertyFyCode -> ParsedFyPyFile:
     property post_marker_file_content using setter
     property fy_py_file_to_parse using setter
     property abstract_property_file_split using abstract_property_regex
+    property parsed_abstract_property_fy_py_file using parsed_fy_py_file
+
 """
 
 from pathlib import Path
 from typing import Any
 
 from base.flow_base import FlowBase
-from domain.fy_py_template_models import AbstractPropertyTemplateModel
-from domain.parsed_fy_py_file import ParsedFyPyFile, ParsedAbstractPropertyFyPyFile
-from domain.python_entity_name import PythonEntityName
+from domain.parsed_fy_py_file import ParsedFyPyFile
+
 from mixins.property.abstract_property_file_split.using_abstract_property_regex_fy import (
     AbstractPropertyFileSplit_UsingAbstractPropertyRegex_PropertyMixin,
 )
@@ -25,6 +26,9 @@ from mixins.property.fy_code.using_setter import (
 )
 from mixins.property.fy_py_file_to_parse.using_setter import (
     FyPyFileToParse_UsingSetter_PropertyMixin,
+)
+from mixins.property.parsed_abstract_property_fy_py_file.parsed_fy_py_file_fy import (
+    ParsedAbstractPropertyFyPyFile_UsingParsedFyPyFile_PropertyMixin,
 )
 from mixins.property.post_marker_file_content.using_setter import (
     PostMarkerFileContent_UsingSetter_PropertyMixin,
@@ -42,38 +46,15 @@ class ParseAbstractPropertyFyCode_Flow(
     PostMarkerFileContent_UsingSetter_PropertyMixin,
     FyPyFileToParse_UsingSetter_PropertyMixin,
     AbstractPropertyFileSplit_UsingAbstractPropertyRegex_PropertyMixin,
+    ParsedAbstractPropertyFyPyFile_UsingParsedFyPyFile_PropertyMixin,
+
     # Base
     FlowBase[ParsedFyPyFile],
 ):
     def __call__(self) -> ParsedFyPyFile:
         # fy:end <<<===
+        return self._parsed_abstract_property_fy_py_file
 
-        assert (
-            len(self._abstract_property_file_split) == 4
-        ), f"Abstract property file split length {len(self._abstract_property_file_split)} is invalid"
-
-        user_imports = self._abstract_property_file_split[0]
-        abstract_property_name = PythonEntityName.from_snake_case(
-            self._abstract_property_file_split[1]
-        )
-        property_type = self._abstract_property_file_split[2]
-
-        parsed_fy_py_file = ParsedAbstractPropertyFyPyFile(
-            fy_code=self._fy_code,
-            pre_marker_file_content=self._pre_marker_file_content,
-            post_marker_file_content=self._post_marker_file_content,
-            file_path=self._fy_py_file_to_parse,
-            user_imports=user_imports,
-            template_model=AbstractPropertyTemplateModel(
-                python_class_name=PythonEntityName.from_pascal_case(
-                    f"With_{abstract_property_name.pascal_case}_PropertyMixin_ABC"
-                ),
-                abstract_property_name=abstract_property_name,
-                property_type=property_type,
-            ),
-        )
-
-        return parsed_fy_py_file
 
     def __init__(
         self,
---
+++ library/src/mixins/property/parsed_abstract_property_fy_py_file/abc_fy.py
@@ -0,0 +1,18 @@
+"""fy
+from domain.parsed_fy_py_file import ParsedAbstractPropertyFyPyFile
+
+
+property parsed_abstract_property_fy_py_file: ParsedAbstractPropertyFyPyFile
+"""
+
+from domain.parsed_fy_py_file import ParsedAbstractPropertyFyPyFile
+import abc
+
+
+# fy:start <<<===
+class With_ParsedAbstractPropertyFyPyFile_PropertyMixin_ABC(abc.ABC):
+    @property
+    @abc.abstractmethod
+    def _parsed_abstract_property_fy_py_file(self) -> ParsedAbstractPropertyFyPyFile:
+        raise NotImplementedError()
+        # fy:end <<<===
