2bc0abe1b1bd336d4d6ad68ff99f25ed7a8565d5
Jere <117933956+PerisicJere@users.noreply.github.com>
2024-08-27 22:12:13 -0400

    implemented property_mixins (#202)
    Signed-off-by: Jere <117933956+PerisicJere@users.noreply.github.com>

---
+++ library/src/mixins/property/property_mixins/abc_fy.py
@@ -0,0 +1,22 @@
+"""fy
+from domain.fy_py_template_models import AbstractPropertyModel
+from typing import List
+
+
+property property_mixins: List[AbstractPropertyModel]
+"""
+
+from domain.fy_py_template_models import AbstractPropertyModel
+from typing import List
+
+
+import abc
+
+
+# fy:start <<<===
+class With_PropertyMixins_PropertyMixin_ABC(abc.ABC):
+    @property
+    @abc.abstractmethod
+    def _property_mixins(self) -> List[AbstractPropertyModel]:
+        raise NotImplementedError()
+        # fy:end <<<===
