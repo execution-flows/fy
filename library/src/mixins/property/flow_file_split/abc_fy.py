669ea0acf1af13c00ba7e9cdb37aaf5c38522d24
GitLee <117933956+PerisicJere@users.noreply.github.com>
2024-08-29 16:39:12 -0400

    refactored flow file split (#235)
    Signed-off-by: GitLee <117933956+PerisicJere@users.noreply.github.com>

---
+++ library/src/mixins/property/flow_file_split/abc_fy.py
@@ -0,0 +1,23 @@
+"""fy
+property flow_file_split: FlowFileSplitModel
+"""
+
+import abc
+
+from pydantic import BaseModel
+
+
+class FlowFileSplitModel(BaseModel):
+    user_imports: str
+    flow_name: str
+    return_type: str
+    mixin_split: str
+
+
+# fy:start ===>>>
+class With_FlowFileSplit_PropertyMixin_ABC(abc.ABC):
+    @property
+    @abc.abstractmethod
+    def _flow_file_split(self) -> FlowFileSplitModel:
+        raise NotImplementedError()
+        # fy:end <<<===
