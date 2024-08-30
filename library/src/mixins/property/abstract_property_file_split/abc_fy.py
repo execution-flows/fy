bef4b39cece36608c19e320f1494d7be0c3762c2
GitLee <117933956+PerisicJere@users.noreply.github.com>
2024-08-30 12:03:43 -0400

    abstract_property_file_split returns pydantic model (#250)

--- library/src/mixins/property/abstract_property_file_split/abc_fy.py
+++ library/src/mixins/property/abstract_property_file_split/abc_fy.py
@@ -2,17 +2,23 @@
 from typing import List
 
 
-property abstract_property_file_split: List[str]
+property abstract_property_file_split: AbstractPropertyFileSplitModel
 """
 
-from typing import List
 import abc
+from pydantic import BaseModel
+
+
+class AbstractPropertyFileSplitModel(BaseModel):
+    user_imports: str
+    abstract_property_name: str
+    property_type: str
 
 
 # fy:start ===>>>
 class With_AbstractPropertyFileSplit_PropertyMixin_ABC(abc.ABC):
     @property
     @abc.abstractmethod
-    def _abstract_property_file_split(self) -> List[str]:
+    def _abstract_property_file_split(self) -> AbstractPropertyFileSplitModel:
         raise NotImplementedError()
         # fy:end <<<===
--- library/src/mixins/property/abstract_property_file_split/using_abstract_property_regex_fy.py
+++ library/src/mixins/property/abstract_property_file_split/using_abstract_property_regex_fy.py
@@ -1,17 +1,19 @@
 """fy
-from typing import List
+from mixins.property.abstract_property_file_split.abc_fy import AbstractPropertyFileSplitModel
 
 
-property abstract_property_file_split: List[str] using abstract_property_regex:
+property abstract_property_file_split: AbstractPropertyFileSplitModel using abstract_property_regex:
     property fy_code
 """
 
 import abc
 import re
 from functools import cached_property
-from typing import List
 
 from constants import FY_ENTITY_REGEX_STRING, PYTHON_MULTI_ENTITY_REGEX_STRING
+from mixins.property.abstract_property_file_split.abc_fy import (
+    AbstractPropertyFileSplitModel,
+)
 from mixins.property.fy_code.abc_fy import (
     With_FyCode_PropertyMixin_ABC,
 )
@@ -24,7 +26,7 @@ class AbstractPropertyFileSplit_UsingAbstractPropertyRegex_PropertyMixin(
     abc.ABC,
 ):
     @cached_property
-    def _abstract_property_file_split(self) -> List[str]:
+    def _abstract_property_file_split(self) -> AbstractPropertyFileSplitModel:
         # fy:end <<<===
         abstract_property_regex = re.compile(
             rf"property\s+(?P<abstract_property_name>{FY_ENTITY_REGEX_STRING})"
@@ -37,4 +39,10 @@ class AbstractPropertyFileSplit_UsingAbstractPropertyRegex_PropertyMixin(
             len(abstract_property_file_split) == 4
         ), f"Abstract property file split length {len(abstract_property_file_split)} is invalid"
 
-        return abstract_property_file_split
+        abstract_property_file_split_model = AbstractPropertyFileSplitModel(
+            user_imports=abstract_property_file_split[0],
+            abstract_property_name=abstract_property_file_split[1],
+            property_type=abstract_property_file_split[2],
+        )
+
+        return abstract_property_file_split_model
