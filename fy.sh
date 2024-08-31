d2c0ec2723348960c63c2d551853d4732cae660b
Vinko Buble <vinkobuble@gmail.com>
2024-08-31 15:23:45 -0400

    fail test when fy fails with own fy code

--- fy.sh
+++ fy.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
+# Instructs Bash to exit immediately if any command in the script returns a non-zero exit code.
+set -e
+
 CURRENT_DIR=$(pwd)
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 FY_CLI_DIR="${SCRIPT_DIR}/cli"
 
-pushd "${FY_CLI_DIR}" >> /dev/null || exit 1
+pushd "${FY_CLI_DIR}" >> /dev/null
 
 poetry run fy --root "$CURRENT_DIR" "$CURRENT_DIR"
 
-EXIT_CODE=$?
-
-popd || exit 1
-
-exit $EXIT_CODE
+popd
