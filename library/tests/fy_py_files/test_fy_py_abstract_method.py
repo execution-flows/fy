a0a41fbe550103b00e8d8577ffb9264407e47135
Vinko Buble <vinkobuble@gmail.com>
2024-08-31 14:33:43 -0400

    regression test for abstract method without properties

--- library/tests/fy_py_files/test_fy_py_abstract_method.py
+++ library/tests/fy_py_files/test_fy_py_abstract_method.py
@@ -8,3 +8,6 @@ from fy_py_files.test_utils.main_fypy_test_case import MainFyPyTestCase
 class TestAbstractMethodFyPyGeneratesFile(MainFyPyTestCase):
     def test_fy_py_abstract_method_generates_files(self) -> None:
         self._test_main_flow(target_folder="abstract_method")
+
+    def test_fy_py_abstract_method_without_arguments_generates_files(self) -> None:
+        self._test_main_flow(target_folder="abstract_method_without_arguments")
---
+++ library/tests/fy_py_files/test_fy_py_files/abstract_method_without_arguments/hello_abstract_method_fy.py.expected
@@ -0,0 +1,13 @@
+"""fy
+method greet -> int
+"""
+
+import abc
+
+
+# fy:start ===>>>
+class Greet_MethodMixin_ABC(abc.ABC):
+    @abc.abstractmethod
+    def _greet(self) -> int:
+        raise NotImplementedError()
+        # fy:end <<<===
