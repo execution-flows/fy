f0d673e255818b5d8b82f98af79cbd78b9e155af
GitLee <117933956+PerisicJere@users.noreply.github.com>
2024-09-01 13:56:40 -0400

    implemented mixin_imports_code (#285)

---
+++ library/src/mixins/property/mixin_imports_code/abc_fy.py
@@ -0,0 +1,17 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+"""fy
+property mixin_imports_code: str
+"""
+
+import abc
+
+
+# fy:start ===>>>
+class MixinImportsCode_PropertyMixin_ABC(abc.ABC):
+    @property
+    @abc.abstractmethod
+    def _mixin_imports_code(self) -> str:
+        raise NotImplementedError()
+        # fy:end <<<===
