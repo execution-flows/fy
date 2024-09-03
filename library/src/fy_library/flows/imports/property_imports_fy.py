5dd93202e419d9b2b0fcbffafbbc098b577354c1
PerisicJere <117933956+PerisicJere@users.noreply.github.com>
2024-09-03 11:46:55 -0400

    finished refactoring ParsedFyPyFileKind.PROPERTY

--- library/src/fy_library/flows/imports/property_imports_fy.py
+++ library/src/fy_library/flows/imports/property_imports_fy.py
@@ -7,11 +7,13 @@ from typing import List
 
 flow PropertyImports -> List[str]:
     property abstract_property_mixins using setter
+    property mixin_import_map using setter
     property cached_import using constant
     property import_abc using when_abstract_property_mixins_exists
+    property import_mixins using for_abstract_property_mixin
 """
 
-from typing import List, Any
+from typing import List, Any, Dict
 
 from fy_core.base.flow_base import FlowBase
 from fy_library.domain.fy_py_template_models import AbstractPropertyModel
@@ -24,14 +26,24 @@ from fy_library.mixins.property.imports.cached_import__using_constant_fy import 
 from fy_library.mixins.property.imports.import_abc__using_when_abstract_property_mixins_exists__fy import (
     ImportAbc_UsingWhenAbstractPropertyMixinsExists_PropertyMixin,
 )
+from fy_library.mixins.property.imports.import_mixins__using_for_abstract_property_mixin__fy import (
+    ImportMixins_UsingForAbstractPropertyMixin_PropertyMixin,
+)
+
+
+from fy_library.mixins.property.mixin_import_map.using_setter import (
+    MixinImportMap_UsingSetter_PropertyMixin,
+)
 
 
 # fy:start ===>>>
 class PropertyImports_Flow(
     # Property Mixins
     AbstractPropertyMixins_UsingSetter_PropertyMixin,
+    MixinImportMap_UsingSetter_PropertyMixin,
     CachedImport_UsingConstant_PropertyMixin,
     ImportAbc_UsingWhenAbstractPropertyMixinsExists_PropertyMixin,
+    ImportMixins_UsingForAbstractPropertyMixin_PropertyMixin,
     # Base
     FlowBase[List[str]],
 ):
@@ -39,11 +51,13 @@ class PropertyImports_Flow(
         self,
         *args: Any,
         abstract_property_mixins: List[AbstractPropertyModel],
+        mixin_import_map: Dict[str, str],
         **kwargs: Any,
     ):
         self._abstract_property_mixins = abstract_property_mixins
+        self._mixin_import_map = mixin_import_map
         super().__init__(*args, **kwargs)
 
     def __call__(self) -> List[str]:
         # fy:end <<<===
-        return self._cached_import + self._import_abc
+        return self._cached_import + self._import_abc + self._import_mixins
