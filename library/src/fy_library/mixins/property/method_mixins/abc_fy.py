e1be5a399b35a44d60c79834f96d4fc84bdfee94
GitLee <jereperisic@icloud.com>
2024-09-04 17:48:32 -0400

    Refactored method mixins import in `ParsedFyPyFileKind.FLOW` (#332)

---
+++ library/src/fy_library/mixins/property/method_mixins/abc_fy.py
@@ -0,0 +1,24 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+"""fy
+from typing import List
+from fy_library.domain.fy_py_template_models import MethodMixinModel
+
+
+property method_mixins: List[MethodMixinModel]
+"""
+
+import abc
+from typing import List
+
+from fy_library.domain.fy_py_template_models import MethodMixinModel
+
+
+# fy:start ===>>>
+class MethodMixins_PropertyMixin_ABC(abc.ABC):
+    @property
+    @abc.abstractmethod
+    def _method_mixins(self) -> List[MethodMixinModel]:
+        raise NotImplementedError()
+        # fy:end <<<===
