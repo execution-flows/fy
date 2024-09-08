1c57b698071c25a3be1a9e520c6d653fd8ded30d
PerisicJere <117933956+PerisicJere@users.noreply.github.com>
2024-09-08 12:32:41 -0400

    implemented abstract_property_template_model

--- library/src/fy_library/flows/create_template_model_using_parsed_fy_py_file/main_fy.py
+++ library/src/fy_library/flows/create_template_model_using_parsed_fy_py_file/main_fy.py
@@ -19,6 +19,9 @@ from fy_library.domain.fy_py_template_models import (
     BaseTemplateModel,
 )
 from fy_library.domain.parsed_fy_py_file import ParsedFyPyFile, ParsedFyPyFileKind
+from fy_library.flows.create_template_model_using_parsed_fy_py_file.abstract_property_template_model_fy import (
+    CreateAbstractPropertyTemplateModel_UsingParsedFyPyFileAndPropertySettersTemplateModels_Flow,
+)
 from fy_library.flows.create_template_model_using_parsed_fy_py_file.base_flow_template_model_fy import (
     CreateBaseFlowTemplateModel_UsingParsedFyPyFileAndPropertySettersTemplateModels_Flow,
 )
@@ -62,6 +65,11 @@ class CreateTemplateModelUsingParsedFyPyFile_Flow(
                 )()
             case ParsedFyPyFileKind.BASE_FLOW:
                 return CreateBaseFlowTemplateModel_UsingParsedFyPyFileAndPropertySettersTemplateModels_Flow(
+                    parsed_fy_py_file=self._parsed_fy_py_file,
+                    parsed_fy_py_files_map_by_key=self._parsed_fy_py_files_map_by_key,
+                )()
+            case ParsedFyPyFileKind.ABSTRACT_PROPERTY:
+                return CreateAbstractPropertyTemplateModel_UsingParsedFyPyFileAndPropertySettersTemplateModels_Flow(
                     parsed_fy_py_file=self._parsed_fy_py_file,
                     parsed_fy_py_files_map_by_key=self._parsed_fy_py_files_map_by_key,
                 )()
