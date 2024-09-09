9221d0ce955dd204776670045ce2d63e34054b20
PerisicJere <117933956+PerisicJere@users.noreply.github.com>
2024-09-09 09:32:42 -0400

    all template models implemented in main_fy flow

--- library/src/fy_library/mixins/property/mixin_import_map/using_parsed_fy_py_files_fy.py
+++ library/src/fy_library/mixins/property/mixin_import_map/using_parsed_fy_py_files_fy.py
@@ -39,7 +39,7 @@ class MixinImportMap_UsingParsedFyPyFiles_PropertyMixin(
     def _mixin_import_map(self) -> Dict[str, str]:
         # fy:end <<<===
         mixin_import_map = {
-            parsed_fy_py_file.template_model.entity_key: self.__parsed_file_python_import(
+            parsed_fy_py_file.entity_key: self.__parsed_file_python_import(  # type: ignore
                 parsed_fy_py_file
             )
             for parsed_fy_py_file in self._parsed_fy_py_files
@@ -53,4 +53,4 @@ class MixinImportMap_UsingParsedFyPyFiles_PropertyMixin(
         )
         file_name = parsed_fy_py_file.file_path.stem
         python_file_path = ".".join(relative_file_folder_path.parts + (file_name,))
-        return f"from {python_file_path} import (\n{' ' * 4}{parsed_fy_py_file.template_model.python_class_name.pascal_case},\n)"
+        return f"from {python_file_path} import (\n{' ' * 4}{parsed_fy_py_file.python_class_name.pascal_case},\n)"
--- library/src/fy_library/mixins/property/parsed_fy_py_files_map_by_key/using_parsed_fy_py_files_fy.py
+++ library/src/fy_library/mixins/property/parsed_fy_py_files_map_by_key/using_parsed_fy_py_files_fy.py
@@ -30,6 +30,6 @@ class ParsedFyPyFilesMapByKey_UsingParsedFyPyFiles_PropertyMixin(
     def _parsed_fy_py_files_map_by_key(self) -> Dict[str, ParsedFyPyFile]:
         # fy:end <<<===
         return {
-            parsed_fy_py_file.template_model.entity_key: parsed_fy_py_file
+            parsed_fy_py_file.entity_key: parsed_fy_py_file  # type: ignore
             for parsed_fy_py_file in self._parsed_fy_py_files
         }
