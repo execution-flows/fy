abf096785204bfabdc4b4b89062a4064c4722a14
PerisicJere <117933956+PerisicJere@users.noreply.github.com>
2024-12-20 14:37:11 -0500

    added tests

---
+++ library/tests/fy_py_files/test_fy_py_files/multi_type_abstract_property/abc_fy.py
@@ -0,0 +1,17 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+"""fy
+property multi_type_abstract_property: dict[str, str]
+"""
+
+import abc
+
+
+# fy:start ===>>>
+class MultiTypeAbstractProperty_PropertyMixin_ABC(abc.ABC):
+    @property
+    @abc.abstractmethod
+    def _multi_type_abstract_property(self) -> dict[str, str]:
+        raise NotImplementedError()
+        # fy:end <<<===
