204e672d5e47d6e15990e7d4e6fbc42ab03a8273
GitLee <jereperisic@icloud.com>
2024-12-21 14:46:07 -0500

    `Flow mixins` use `method` and `property` with `generics_impl` (#433)

--- library/src/fy_library/mixins/property/optional_property_mixin_model/using_mixin_line_fy.py
+++ library/src/fy_library/mixins/property/optional_property_mixin_model/using_mixin_line_fy.py
@@ -14,7 +14,10 @@ import re
 from functools import cached_property
 from typing import Final
 
-from fy_library.constants import FY_ENTITY_REGEX_STRING
+from fy_library.constants import (
+    FY_ENTITY_REGEX_STRING,
+    PYTHON_MULTI_ENTITY_REGEX_STRING,
+)
 from fy_library.domain.mixin_models import MixinModelKind, PropertyMixinModel
 from fy_library.domain.python_entity_name import PythonEntityName
 from fy_library.mixins.property.mixin_line.abc_fy import (
@@ -23,7 +26,8 @@ from fy_library.mixins.property.mixin_line.abc_fy import (
 
 _FLOW_PROPERTY_REGEX: Final = re.compile(
     rf"^\s+property\s+(?P<property_name>{FY_ENTITY_REGEX_STRING})\s+"
-    rf"using\s+(?P<implementation_name>{FY_ENTITY_REGEX_STRING})\s*$"
+    rf"using\s+(?P<implementation_name>{FY_ENTITY_REGEX_STRING})\s*"
+    rf"(?:\[(?P<generics_impl>{PYTHON_MULTI_ENTITY_REGEX_STRING})])?$"
 )
 
 
@@ -47,6 +51,7 @@ class OptionalPropertyMixinModel_UsingMixinLine_PropertyMixin(
         implementation_name: PythonEntityName = PythonEntityName.from_snake_case(
             flow_property_fy_search.group("implementation_name")
         )
+
         return PropertyMixinModel(
             python_class_name=PythonEntityName.from_pascal_case(
                 f"{ property_name.pascal_case }_Using{ implementation_name.pascal_case }_PropertyMixin"
@@ -54,5 +59,5 @@ class OptionalPropertyMixinModel_UsingMixinLine_PropertyMixin(
             kind=MixinModelKind.PROPERTY,
             property_name=property_name,
             implementation_name=implementation_name,
-            generics_impl="",
+            generics_impl=flow_property_fy_search.group("generics_impl") or "",
         )
---
+++ library/tests/fy_py_files/test_fy_py_files/flow_with_generics_impl/method/method_fy.py
@@ -0,0 +1,21 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+"""fy
+from ..greetings_t import GreetT
+
+
+method greet[GreetT](message: str) -> str using greeting:
+"""
+
+from typing import Generic
+from ..greetings_t import GreetT
+
+
+# fy:start ===>>>
+class Greet_UsingGreeting_MethodMixin(
+    Generic[GreetT],
+):
+    def _greet(self, message: str) -> str:
+        # fy:end <<<===
+        return message
