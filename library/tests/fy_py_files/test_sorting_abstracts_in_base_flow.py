af0f69ebd8ee7ee2c82f058b58a13ade661f3d7a
PerisicJere <117933956+PerisicJere@users.noreply.github.com>
2024-12-26 20:09:58 -0500

    fixed base flow ordering test

--- library/tests/fy_py_files/test_sorting_abstracts_in_base_flow.py
+++ library/tests/fy_py_files/test_sorting_abstracts_in_base_flow.py
@@ -8,11 +8,11 @@ class TestSortingAbstractsInBaseFlow(MainFyPyTestCase):
     def test_sorting_abstracts_in_base_flow(self) -> None:
         self._test_main_flow(target_folder="base_flow_ordering_abstract_mixins")
 
-        from .test_fy_py_files.ordered_abstract_methods_test.flow_fy import (
+        from .test_fy_py_files.base_flow_ordering_abstract_mixins.flow_fy import (
             Message_Flow,
         )
 
         self.assertEqual(
             Message_Flow()(),
-            "required_1required_2required_2required_1required_1required_2",
+            "required_1required_2required_1required_2",
         )
