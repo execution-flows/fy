700b9ae966e1ba068a0de086b94283109c24855d
PerisicJere <117933956+PerisicJere@users.noreply.github.com>
2024-12-30 15:10:47 -0500

    added enum for annotations

--- library/src/fy_library/domain/annotation_object.py
+++ library/src/fy_library/domain/annotation_object.py
@@ -1,9 +1,15 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+from enum import Enum
 
 from pydantic import BaseModel
 
 
+class AnnotationKind(Enum):
+    CALLABLE = "callable"
+
+
 class Annotation(BaseModel):
+    kind: AnnotationKind
     name: str
