77f9148b6f08d17dc32abd2f66047d4fd2b4ab88
GitLee <jereperisic@icloud.com>
2024-12-31 14:10:00 -0500

    Raises an error when two of the same mixins are added to entity (#486)

--- library/tests/fy_py_files/test_invalid_fy_code_raises_error.py
+++ library/tests/fy_py_files/test_invalid_fy_code_raises_error.py
@@ -67,3 +67,13 @@ class TestRaisesAssertionError(MainFyPyTestCase):
                 target_folder="invalid_fy_code_raises_correct_error/property_has_duplicate_name",
                 perform_fy_code_deletion=False,
             )
+
+    def test_flow_uses_multiple_same_entities(self) -> None:
+        with self.assertRaisesRegex(
+            expected_exception=AssertionError,
+            expected_regex=r"Duplicate found in .*.",
+        ):
+            self._test_main_flow(
+                target_folder="invalid_fy_code_raises_correct_error/flow_uses_multiple_same_entities",
+                perform_fy_code_deletion=False,
+            )
