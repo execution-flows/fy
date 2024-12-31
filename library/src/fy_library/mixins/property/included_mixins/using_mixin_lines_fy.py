5ef374d6dedd1b0a5808a2a704313a1c1edae0b9
PerisicJere <117933956+PerisicJere@users.noreply.github.com>
2024-12-31 12:19:03 -0500

    Raises an error when two of the same mixins are added to entity

--- library/src/fy_library/mixins/property/included_mixins/using_mixin_lines_fy.py
+++ library/src/fy_library/mixins/property/included_mixins/using_mixin_lines_fy.py
@@ -12,7 +12,7 @@ fy"""
 import abc
 from functools import cached_property
 
-from fy_library.domain.mixin_models import MixinModelKind
+from fy_library.domain.mixin_models import MixinModelKind, BaseMixinModel
 from fy_library.mixins.property.included_mixins.abc_fy import (
     IncludedMixinsModel,
     IncludedMixins_PropertyMixin_ABC,
@@ -32,6 +32,14 @@ class IncludedMixins_UsingMixinModels_PropertyMixin(
     @cached_property
     def _included_mixins(self) -> IncludedMixinsModel:
         # fy:end <<<===
+        check_for_duplicates_dict: dict[str, BaseMixinModel] = {}
+        for mixin_model in self._mixin_models:
+            if mixin_model.entity_key not in check_for_duplicates_dict:
+                check_for_duplicates_dict[mixin_model.entity_key] = mixin_model
+            else:
+                raise AssertionError(
+                    f"Duplicate found in {mixin_model.entity_key, mixin_model.kind.name}."
+                )
 
         included_mixins = IncludedMixinsModel(
             abstract_method_mixins=[
