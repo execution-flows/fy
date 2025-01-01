3bfded474d4868db08c069d0b741cbcec0b2846d
Vinko Buble <vinkobuble@gmail.com>
2025-01-01 00:35:51 -0500

    fixed abstract mixins with generics ordering (#490)

--- library/src/fy_library/mixins/property/ordered_abstract_entities/using_parsed_fy_py_files_map_by_key_fy.py
+++ library/src/fy_library/mixins/property/ordered_abstract_entities/using_parsed_fy_py_files_map_by_key_fy.py
@@ -11,11 +11,15 @@ fy"""
 
 from functools import cached_property
 
-from typing import Dict
+from typing import Dict, cast
 
 import abc
 
-from fy_library.domain.parsed_fy_py_file import ParsedFyPyFileKind
+from fy_library.domain.parsed_fy_py_file import (
+    ParsedFyPyFileKind,
+    ParsedAbstractPropertyFyPyFile,
+    ParsedAbstractMethodFyPyFile,
+)
 from fy_library.mixins.property.parsed_fy_py_files_map_by_key.abc_fy import (
     ParsedFyPyFilesMapByKey_PropertyMixin_ABC,
 )
@@ -35,10 +39,26 @@ class AbstractEntitiesOrderingIndex_UsingParsedFyPyFilesMapByKey_PropertyMixin(
     @cached_property
     def _abstract_entities_ordering_index(self) -> Dict[str, int]:
         # fy:end <<<===
+        def sorting_mixin_key(mixin_key: str) -> str:
+            parsed_fy_py_file = self._parsed_fy_py_files_map_by_key[mixin_key]
+            match parsed_fy_py_file.file_type:
+                case ParsedFyPyFileKind.ABSTRACT_PROPERTY:
+                    abstract_property_parsed_fy_py_file = cast(
+                        ParsedAbstractPropertyFyPyFile, parsed_fy_py_file
+                    )
+                    return f"{0 if len(abstract_property_parsed_fy_py_file.generics_def) == 0 else 1}-{mixin_key}"
+                case ParsedFyPyFileKind.ABSTRACT_METHOD:
+                    abstract_method_parsed_fy_py_file = cast(
+                        ParsedAbstractMethodFyPyFile, parsed_fy_py_file
+                    )
+                    return f"{0 if len(abstract_method_parsed_fy_py_file.generics_def) == 0 else 1}-{mixin_key}"
+                case _:
+                    return f"0-{mixin_key}"
+
         abstract_entities_ordering_index = {
             entity_key: entity_num
             for entity_num, entity_key in enumerate(
-                sorted(self._parsed_fy_py_files_map_by_key)
+                sorted(self._parsed_fy_py_files_map_by_key, key=sorting_mixin_key)
             )
             if self._parsed_fy_py_files_map_by_key[entity_key].file_type
             in {
