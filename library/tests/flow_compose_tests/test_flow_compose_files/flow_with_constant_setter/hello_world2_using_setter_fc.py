724d9469da83dba4fd5062df2c9bcd6cf14a4211
Vinko Buble <vinkobuble@gmail.com>
2025-02-12 18:37:53 -0500

    fixed failing tests

--- library/tests/flow_compose_tests/test_flow_compose_files/flow_with_constant_setter/hello_world2_using_setter_fc.py
+++ library/tests/flow_compose_tests/test_flow_compose_files/flow_with_constant_setter/hello_world2_using_setter_fc.py
@@ -1,10 +1,10 @@
-from flow_compose import flow, FlowArgument, FlowFunction
+from flow_compose import flow, FlowArgument
 
 
 @flow(
     greeting=FlowArgument(str),
 )
 def hello_world2(
-    greeting: FlowFunction[str],
+    greeting: FlowArgument[str],
 ) -> str:
     return greeting()
--- library/tests/flow_compose_tests/test_flow_compose_files/flow_with_constant_setter/hello_world_using_constant_fc.py
+++ library/tests/flow_compose_tests/test_flow_compose_files/flow_with_constant_setter/hello_world_using_constant_fc.py
@@ -1,10 +1,10 @@
-from flow_compose import flow, FlowArgument, FlowFunction
+from flow_compose import flow, FlowArgument
 
 
 @flow(
     greeting=FlowArgument(str, value="Hello World"),
 )
 def hello_world(
-    greeting: FlowFunction[str],
+    greeting: FlowArgument[str],
 ) -> str:
     return greeting()
