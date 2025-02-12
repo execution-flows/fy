d6aed5787c5c3ef951deba3a943de5a36b2aa519
Vinko Buble <vinkobuble@gmail.com>
2025-02-11 22:57:59 -0500

    --fc flag

---
+++ library/src/fy_library/fc.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+from fy_library.flows.flow_compose_main_fy import FlowCompose_Main_Flow
+
+
+def fc(
+    folder_to_parse: Path,
+    project_root_folder: Path,
+) -> None:
+    FlowCompose_Main_Flow(
+        folder_to_parse=Path(folder_to_parse),
+        project_root_folder=(
+            Path(project_root_folder) if project_root_folder is not None else Path.cwd()
+        ),
+    )()
